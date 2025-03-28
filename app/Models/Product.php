f6c3716d1b0f9634059fee4260475d293c248612
Alvin Pusta <alvin.pusta@altern8.sg>
2025-03-28 21:44:01 +0800

    #2: feat: product model, migration and factory

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'unit_id',
+        'is_ingredient',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_ingredient' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
+
+    public function scopeIngredient($query)
+    {
+        return $query->where('is_ingredient', true);
+    }
+}
