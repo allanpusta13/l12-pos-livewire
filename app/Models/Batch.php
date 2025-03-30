716ae0ab63b86a1ae5288dc5322ae6b691ae3610
Alvin Pusta <alvin.pusta@altern8.sg>
2025-03-31 00:39:48 +0800

    #3: feat: add batch model, factory and migration

---
+++ app/Models/Batch.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Batch extends Model
+{
+    /** @use HasFactory<\Database\Factories\BatchFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'batchable_id',
+        'batchable_type',
+        'location_id',
+        'batch_number',
+        'expiry_date',
+        'status',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+}
